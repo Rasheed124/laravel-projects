9f142ea26c8b3b9133617e13aa01d9bc74885293
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-03-29 15:03:37 +0100

    working contacts app

---
+++ app/Models/Company.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    // protected $table = "app_companies";
+    // protected $primaryKey = "_id";
+    protected $fillable = ['name', 'email', 'address', 'website'];
+
+    
+
+    // public function contacts()
+    // {
+    //     return $this->hasMany(Contact::class);
+    // }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(CompanySeeder::class);
+        // $this->call(CompanySeeder::class);
+        $this->call([
+            CompanySeeder::class,
+            ContactSeeder::class,
+        ]);
     }
 }
